52dcde00a960745eacb220792a439b3a003c0c7b
ggiesa <ggiesawilson@gmail.com>
2017-05-09 23:15:11 -0700

    Added functioning zoom and some comments

--- mandlebrot.py
+++ mandlebrot.py
@@ -17,6 +17,10 @@ class mandlebrot(QWidget):
         self.releasePos = None
         self.a = None
         self.b = None
+        self.xintervalStart = None
+        self.xintervalEnd = None
+        self.yintervalStart = None
+        self.yintervalEnd = None
 
 
 
@@ -37,25 +41,39 @@ class mandlebrot(QWidget):
     def drawPoints(self, qp):
 
         size = self.size()
-        print(size)
+
+        # If user has selected an area to zoom in on, calculate new interval to iterate function over
         if self.releasePos:
-            width = self.releasePos[0] - self.pressPos[0]
-            height = width*(size.height()/size.width())
+            x1 = self.xintervalStart + (self.xintervalEnd - self.xintervalStart)*((self.pressPos[0]-1)/(size.width()-1))
+            x2 = self.xintervalStart + (self.xintervalEnd - self.xintervalStart)*((self.releasePos[0]-1)/(size.width()-1))
+            y1 = self.yintervalStart - (self.yintervalStart - self.yintervalEnd)*((self.pressPos[1]-1)/(size.height()-1))
+            y2 = self.yintervalStart - (self.yintervalStart - self.yintervalEnd)*((self.releasePos[1]-1)/(size.height()-1))
+
+            self.xintervalStart = x1
+            self.xintervalEnd = x2
+            self.yintervalStart = y1
+            self.yintervalEnd = y2
+
+        # If program has just been initialized, use the fully 'zoomed out' interval:  (-2 < x < 1) and (-1 < y < 1)
         else:
-            width = size.width()
-            height = size.height()
-            self.a = -2
-            self.b = 1
+            self.xintervalStart = -2
+            self.xintervalEnd = 1
+            self.yintervalEnd = -1
+            self.yintervalStart = 1
 
+            x1 = self.xintervalStart
+            x2 = self.xintervalEnd
+            y1 = self.yintervalStart
+            y2 = self.yintervalEnd
 
+        # i and ii loop over every horizontal and vertical pixel coordinate in the widget, filling in an image. x and y are the
+        # actual values that are used in the mandlebrot function. For each pixel, mandlebrotColor is called, iterating the
+        # mandlebrot function and assigning the pixel a color based on the 'speed' at which the function diverges
         for i in range(1, size.width() -1):
-
-            x = self.a + 3*((i-1)/(size.width()-1))
+            x = x1 + (x2 - x1)*((i-1)/(size.width()-1))
 
             for ii in range( 1, size.height() -1):
-
-                y = self.b - 2*((ii-1)/(height-1))
-
+                y = y1 - (y1 - y2)*((ii-1)/(size.height()-1))
 
                 color = self.mandlebrotColor(x,y)
                 qp.setPen(QColor(color[0], color[1], color[2]))
